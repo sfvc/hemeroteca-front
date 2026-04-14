ed99f3e00459f481914483cbf7fbcbc19ab515ad
Nahuewe <nahuelsoriap@gmail.com>
2026-04-14 12:54:12 -0300

    visor completado

--- src/pages/Colecciones.tsx
+++ src/pages/Colecciones.tsx
@@ -340,7 +340,7 @@ export default function Colecciones() {
         ))}
       </div>
 
-      <div className="grid gap-6 sm:grid-cols-2 xl:grid-cols-4 mb-12">
+<div className="grid grid-cols-2 gap-4 sm:grid-cols-2 lg:grid-cols-4 mb-12">
         {filtros.map((f) => (
           <FiltroPrincipalCard
             key={f.id}
